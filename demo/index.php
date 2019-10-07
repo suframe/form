e6937009cc849514765eb1cda969eb47c0c553c3
shulinqian <330576744@qq.com>
2019-10-07 17:20:13 +0800

    增加返回

--- demo/index.php
+++ demo/index.php
@@ -1,7 +1,16 @@
 <?php
+use FormBuilder\Response;
+
 $loader = require __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader->addPsr4('demo\\', __DIR__ . DIRECTORY_SEPARATOR . 'demo');
+
+if($_POST){
+    header("content:application/json;chartset=uft-8");
+    $response = Response::success('表单提交成功sdsdxcvxc');
+    $response->send();
+    exit;
+}
 
 $form = new \suframe\form\Form();
 $form->createElm();
